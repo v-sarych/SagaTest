ec4b188b2d593452e9374f0f9a6b72b06e783f6a
drr <drr@DESKTOP-4VLSIMM>
2024-11-21 17:53:22 +0300

    x

---
+++ Domain/Services/IPaymentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public interface IPaymentService
+    {
+        Task StartPayment();
+        Task AcceptPayment();
+
+        Task Refund();
+    }
+}
