bf09760907272449c6f59e7350627ed886d41a9d
drr <drr@DESKTOP-4VLSIMM>
2024-12-02 16:55:13 +0300

    .

---
+++ RabbitWrapper/Abstractions/IRabbitRpcConsumer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitWrapper.Abstractions
+{
+    public interface IRabbitRpcConsumer
+    {
+        Task Consume<TResponse, TRequest>(string procedureName, Func<TRequest, Task<TResponse>> handler);
+    }
+}
---
+++ RabbitWrapper/Models/RpcResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitWrapper.Models
+{
+    public class RpcResponse<TData>
+    {
+        public Guid CorrelationId { get; set; }
+        public bool IsSuccess { get; set; }
+
+        public string? Eror {  get; set; }
+        public TData? Data { get; set; }
+    }
+}
